3f1d1aa86e6477ccac9d8cf29505c4c8f4424b5d
LIU_Code <cmdegr@gmail.com>
2019-01-28 11:33:44 +0800

    实现转换为int,并且把int取出个位,十位,百位....放到数组中 把数组的值放到ListNode中.

--- Number2.java
+++ Number2.java
@@ -15,6 +15,7 @@
      * 原因：342 + 465 = 807
  */
 public class Number2 {
+    public static ListNode sumTwoNode = new ListNode();
     /**
      * @param l1
      * @param l2
@@ -22,15 +23,64 @@ public class Number2 {
      * 内容: 处理ListNode的主程序
      */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode sumTwoNode = new ListNode();
+        //ListNode sumTwoNode = new ListNode();
+        //把值放到sumTwoNodeInt 中,把反向取余数的值放到一个数组中
+        int sumTwoNodeInt = 0;
+        //int []a = new int [100];
         //把l1和l2的值转换为10进制的int
         int valueListNodeL1 = 0;
         int valueListNodeL2 = 0;
 
         valueListNodeL1 = getValueListNode(l1);
         valueListNodeL2 = getValueListNode(l2);
-        System.out.print(valueListNodeL1+","+valueListNodeL2);
+        System.out.println(valueListNodeL1+","+valueListNodeL2);
+
+        //两个已经转换为int类型的数据相加,把和进行反向取余数,并且把余数放到listNode中
+        sumTwoNodeInt= valueListNodeL1 + valueListNodeL2;
+        System.out.println(sumTwoNodeInt);
+        int length=getNumLenght(sumTwoNodeInt);
+        int []a = new int [length];
+        ListNode []b = new ListNode[length];
+        for (int i =0;i<length;i++){
+            //sumTwoNodeInt/10的多少次方%10  从10的0次方开始
+            a[i]= (int) (sumTwoNodeInt/(Math.pow((double)10,(double)i))%10);
+            //i+1<length的不等式成立表示有下一个,并且把next指向下一个
+            if(i+1<length){
+                b[i]=new ListNode(a[i]);
+                b[i].setNext(b[i+1]);
+                //b[i].setNext(b[i+1]);
+                //b[i].setVal(a[i]);
+            }else {
+                b[i]=new ListNode(a[i]);
+                b[i].setNext(null);
+                //b[i].setNext(null);
+                //b[i].setVal(a[i]);
+            }
+        }
+
+        //循环输出a
+        for (int i : a) {
+            System.out.println(i);
+        }
+
         return sumTwoNode;
+    }
+
+    /**
+     * 取出一个数的位数
+     * @param num
+     * @return
+     */
+    private static int getNumLenght(long num){
+        if (num==0) {
+            return 1;
+        }
+        int lenght = 0;
+        long temp =0;
+        for (temp = num ; temp != 0; temp/=10){
+            lenght++;
+        }
+        return lenght;
     }
 
     /**
@@ -44,7 +94,7 @@ public class Number2 {
         //值=Val的10的多少次方 累加
         int i=0;
         if (lNode !=null){
-
+            //设置两个指针,一个用于计算,一个用于指向下一个的地址并且进行判断是否为空
             do {
                 ListNode lNode2 = lNode;
                 sum+= lNode2.getVal() *(int)(Math.pow((double) 10,(double) i));
@@ -67,11 +117,15 @@ public class Number2 {
         ListNode l1 = new ListNode(1);
         ListNode l12 = new ListNode(1);
         ListNode l13 = new ListNode(1);
-
         l1.setNext(l12);
         l12.setNext(l13);
 
         ListNode l2 = new ListNode(2);
+        ListNode l22 = new ListNode(3);
+        ListNode l23 =new ListNode(5);
+        l2.setNext(l22);
+        l22.setNext(l23);
+
         addTwoNumbers(l1,l2);
     }
 
