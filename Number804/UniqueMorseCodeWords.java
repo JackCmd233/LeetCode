cce9e3f12bbce0d299b631c82e9cbef8050e13ce
LIU_Code <cmdegr@gmail.com>
2019-02-05 18:00:28 +0800

    no message

--- Number804/UniqueMorseCodeWords.java
+++ Number804/UniqueMorseCodeWords.java
@@ -2,6 +2,42 @@ package Number804;
 
 public class UniqueMorseCodeWords {
     public int uniqueMorseRepresentations(String[] words) {
+        int a =0;
+        return a;
+    }
 
+    public static void main(String []args){
+        Zi2 z2 = new Zi2();
+
+    }
+}
+
+abstract class AbstractFu{
+    int fuNumber ;
+    String fuName;
+
+    abstract void show(String  name);
+
+
+}
+
+abstract class AbstractZi extends AbstractFu {
+    int ziNumber;
+    String ziName;
+
+    @Override
+    void show(String name) {
+        System.out.println("ziname");
+    }
+
+    abstract void show(int number);
+
+}
+
+class Zi2 extends AbstractZi {
+
+    @Override
+    void show(int number) {
+        System.out.println("ziziname");
     }
 }
