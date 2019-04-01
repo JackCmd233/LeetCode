5e587829dedca1589cf1ac4d3eb4bad934349463
CMDEGR <cmdegr@gmail.com>
2019-04-01 22:28:02 +0800

    Number15

--- Number15/Solution1OfNum15.java
+++ Number15/Solution1OfNum15.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 //超时,算法没问题,时间复杂度太大
+
 /**
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
  * 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
@@ -42,24 +43,17 @@ public class Solution1OfNum15 {
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        for (int temp : nums) {
-            System.out.print(temp + ",");
-        }
-        System.out.println("");
 
         List<List<Integer>> listResult = new LinkedList<>();
         int temp = 0;
-        List<Integer> tempNode = new LinkedList<Integer>();
+        List<Integer> tempNode = new LinkedList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
-                //System.out.println("............");
                 temp = nums[i] + nums[j];
                 for (int z = j + 1; z < nums.length; z++) {
-                    //System.out.println("============");
                     if (temp == (0 - nums[z])) {
                         tempNode = new LinkedList<>();
-                        //System.out.println(nums[i]+","+nums[j]+","+nums[z]+",");
                         tempNode.add(nums[i]);
                         tempNode.add(nums[j]);
                         tempNode.add(nums[z]);
@@ -67,15 +61,12 @@ class Solution {
                         if (!listResult.contains(tempNode)) {
                             listResult.add(tempNode);
                         }
-
                     }
                     //break;
                 }
             }
         }
 
-        //tempNode.add(3);
-        //listResult.add(tempNode);
         return listResult;
     }
 }
