2f0d2ad297b75eeb67deb743e3d45f6ad4e02b71
liuyicai <1914516442@qq.com>
2022-12-09 18:11:54 +0800

    尝试提交

---
+++ src/main/java/leetcode/editor/cn/NumOffer06_CongWeiDaoTouDaYinLianBiaoLcof.java
@@ -0,0 +1,64 @@
+package leetcode.editor.cn;
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * <p>
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 10000
+ * <p>
+ * Related Topics 栈 递归 链表 双指针 👍 356 👎 0
+ */
+
+
+/**
+ * 时间：2022-12-09 18:09:52
+ * 题目编号：剑指 Offer 06
+ * 题目标题：从尾到头打印链表
+ */
+public class NumOffer06_CongWeiDaoTouDaYinLianBiaoLcof {
+
+    public static void main(String[] args) {
+        //Solution solution = new Num剑指 Offer 06_CongWeiDaoTouDaYinLianBiaoLcof().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int[] reversePrint(ListNode head) {
+            return new int[]{};
+        }
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
